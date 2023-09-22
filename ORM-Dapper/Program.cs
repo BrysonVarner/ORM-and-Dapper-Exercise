0bd4d9a3350d495972eee763915094d9b7b9c1dc
Bryson <brysonandmellisa@hotmail.com>
2023-09-22 18:39:48 -0500

    completed ORM and Dapper Exercise with Extras

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -17,7 +17,36 @@ namespace ORM_Dapper
 
             IDbConnection conn = new MySqlConnection(connString);
 
-            var departmentRepo = new DapperDepartmentRepository(conn);
+            var repo = new DapperProductRepository(conn);
+
+            //Add item here
+            //repo.CreateProduct("newItems", 20, 1);
+
+            //Change item name
+            //repo.UpdateProductName(940, "Delete Me Next");
+
+            //Update All Product info
+            var productToUpdate = repo.GetProductByID(940);
+
+            productToUpdate.Name = "Corsair Scimitar RGB Elite";
+            productToUpdate.Price = 79.99m;
+            productToUpdate.CategoryID = 2;
+            productToUpdate.OnSale = 1;
+            productToUpdate.StockLevel = "250";
+
+            repo.UpdateAllProductFields(productToUpdate);
+
+            // delete items added
+            //repo.DeleteProduct(941);
+
+
+
+            var products = repo.GetAllProducts();
+
+            foreach (var prod in products)
+            {
+                Console.WriteLine($"{prod.ProductID} {prod.Name}");
+            }
         }
     }
 }
